61c2e23f75603cbf8c18f11f3a8144954678f2f9
Christian Capon <107329400+Chrlstian@users.noreply.github.com>
2023-11-22 20:49:24 +0800

    add new data,create certificate page, and add cert images

--- src/components/CertCard.jsx
+++ src/components/CertCard.jsx
@@ -1,9 +1,30 @@
 
-export default function CertCard(props){
-    
-    return(
-        <div className={`border border-gray-500 p-4 flex flex-col gap-4 rounded bg-primary hover:border-0 ${props.lightMode ? 'hover:shadow-lg hover:shadow-black' : 'hover:shadow-lg hover:shadow-btnB'}`}>
+import { useState } from "react";
+
+export default function CertCard(props) {
+    const [isHovered, setIsHovered] = useState(false);
+  
+    return (
+      <div
+            className={`border border-gray-500 p-4 flex flex-col gap-4 rounded bg-primary relative ${props.lightMode ? 'hover:shadow-lg hover:shadow-black' : 'hover:shadow-lg hover:shadow-btnB'}`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+      >
+        
             <img src={props.item.img} alt="Certificate of Christian Benson Capon" />
-        </div>
+        
+            {isHovered && (
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20">
+                    <a
+                        href={props.item.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="bg-primary text-gray-200 px-4 py-2 rounded" 
+                    > 
+                        Show
+                    </a>
+                </div>
+            )}
+      </div>
     );
-}
+  }
--- src/data/certificatesData.jsx
+++ src/data/certificatesData.jsx
@@ -1,11 +1,59 @@
 
+import cert1 from '../assets/images/certificates/cert-1.webp';
+import cert2 from '../assets/images/certificates/cert-2.webp';
+import cert3 from '../assets/images/certificates/cert-3.webp';
+import cert4 from '../assets/images/certificates/cert-4.webp';
+import cert5 from '../assets/images/certificates/cert-5.webp';
+import cert6 from '../assets/images/certificates/cert-6.webp';
+import cert7 from '../assets/images/certificates/cert-7.webp';
+import cert8 from '../assets/images/certificates/cert-8.webp';
+import cert9 from '../assets/images/certificates/cert-9.webp';
+
 export default [
     {
         id: 1,
-        img: "This is a sample"
+        img: cert1,
+        url: 'https://drive.google.com/file/d/1G9Dym5ezNdCE_QYnToKgEGnfB3B72bQZ/view?usp=sharing'
     },
     {
         id: 2,
-        img: "This is a sample"
+        img: cert2,
+        url: 'https://drive.google.com/file/d/1P-7hmVLTJHyOyqKuz9c9oVaQdQbd6Cwj/view?usp=sharing'
+    },
+    {
+        id: 3,
+        img: cert3,
+        url: 'https://drive.google.com/file/d/11cAjmsigxP2Th-6dJwzo224rfVBr1iTc/view?usp=sharing'
+    },
+    {
+        id: 4,
+        img: cert4,
+        url: 'https://drive.google.com/file/d/1CuaqAeP-bNNEuF-iLDv1JdDI3WEDjjLn/view?usp=sharing'
+    },
+    {
+        id: 5,
+        img: cert5,
+        url: 'https://drive.google.com/file/d/12IX-_YmxYHHIs3wkTlZbP2cgb1koJsuE/view?usp=sharing'
+    },
+    {
+        id: 6,
+        img: cert6,
+        url: 'https://drive.google.com/file/d/1wIdOGOYnRr6ZJ137auTx_zX_3kBodR6v/view?usp=sharing'
+    },
+    {
+        id: 7,
+        img: cert7,
+        url: 'https://drive.google.com/file/d/1ingaMO2vvQ-sAuoXtglKAT4PXAG1I4Pl/view?usp=sharing'
+    },
+    {
+        id: 8,
+        img: cert8,
+        url: 'https://drive.google.com/file/d/1w-q-bpsNaDUvU5GlQgHxBNFFjNcjGatj/view?usp=sharing'
+    },
+    {
+        id: 9,
+        img: cert9,
+        url: 'https://drive.google.com/file/d/1U9f7m7j8S-wdbXLIDFwqc6bCm1aGk5JZ/view?usp=sharing'
     }
+
 ];
