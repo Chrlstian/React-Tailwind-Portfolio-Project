0e43733fc7d1cc38c0ba19dfed5f2a2d00019d2c
Christian Capon <107329400+Chrlstian@users.noreply.github.com>
2023-11-26 17:52:59 +0800

    fix the error in downloading the resume pdf

--- src/components/DownloadResume.jsx
+++ src/components/DownloadResume.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import downloadIcon from '../assets/images/download-icon.webp';
+import resume from '../assets/resumePdf/christianbensoncapon.pdf';
 
-const DownloadButton = () => {
-  const handleDownload = async () => {
-    const pdfUrl = '../assets/resumePdf/christianbensoncapon.pdf'; // Update this with the correct path
-
-    try {
-      const response = await fetch(pdfUrl);
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', 'christianCapon.pdf');
-      document.body.appendChild(link);
-      link.click();
-    } catch (error) {
-      console.error('Error downloading the resume', error);
-    }
-  };
-
+export default function DownloadButton(){
+  
   return (
-    <div className='flex gap-4 justify-center cursor-pointer hero-btn-1 sm:w-full lg:w-full lg:text-lg' onClick={handleDownload}>
+    <a 
+      className='flex gap-4 justify-center cursor-pointer hero-btn-1 sm:w-full lg:w-full lg:text-lg'
+      download="ChristianBensonCapon.pdf" 
+      href={resume} 
+      target='_blank' 
+      rel='noopener noreferrer' >
+      
       <p>Download Resume</p>
       <img src={downloadIcon} alt="download icon" />
-    </div>
+      
+    </a>
+    
   );
 };
 
-export default DownloadButton;
+
+
