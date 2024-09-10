4b2e5862351b319cb313aad898d16bab6b810714
PC <cbbc0000@gmail.com>
2024-09-10 19:09:38 +0800

    update the home content

--- src/pages/nav/Home.jsx
+++ src/pages/nav/Home.jsx
@@ -8,7 +8,7 @@ export default function Home(props) {
     <div className={`heroContainer-md max-w-screen-2xl 2xl:m-auto ${props.lightMode ? 'bg-white' : 'bg-black'}`}>
       <div className='hero-info sm:gap-10 lg:pl-8 xl:p-20 xl:max-w'>
         <h1 className={`font-body text-4xl text-center sm:text-5xl lg:text-start lg:text-6xl ${props.lightMode ? 'text-black md:text-secondary' : 'text-gray-200' }`}>Helping people make the world a better place through quality <span className='text-pink-700'>web solutions.</span></h1>
-        <p className='text-center text-txt md:text-lg'>"Hi, I'm Christian, an entry Junior software developer excited to build amazing things for you."</p>
+        <p className='text-center text-txt md:text-lg'>"Hi, I'm Christian, a Junior software developer excited to build amazing things for you."</p>
         <div className='flex flex-col gap-3 sm:flex-row sm:justify-between md:flex-col lg:flex-row hero-btn-lg'>
           <Link to="projects" className='hero-btn-0 sm:w-full lg:w-full lg:text-lg text-center'><button>View Projects</button></Link>
           <ViewResume />
